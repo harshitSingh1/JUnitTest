127d4a336ce0fa831562627448cee7179ab4ac22
harshitSingh1 <sharshitsingh007@gmail.com>
2022-11-16 17:35:44 +0530

    J Unit Test

--- src/JUnitTestPackage/JUnitFunctions.java
+++ src/JUnitTestPackage/JUnitFunctions.java
@@ -1,27 +1,10 @@
 package JUnitTestPackage;
 public class JUnitFunctions {
-	    public static void main(String[] args) {
-	        int i,j;
-	        String s_arr[] = {
-	            "Richard",
-	            "John",
-	            "Williams",
-	            "Peter",
-	            "Aron"
-	        };
-	        String tmp;
-	        System.out.println("Sorted Strings:");
-	        for ( i = 0 ; i < s_arr.length ; i++) {
-	            for ( j = 0 ; j < s_arr.length-i-1 ; j++) {
-
-	                if (s_arr[j+1].compareTo(s_arr[j]) > 0) {
-	                    tmp = s_arr[j];
-	                    s_arr[j] = s_arr[j+1];
-	                    s_arr[j+1] = tmp;
-	                }
-	            }
-	            System.out.println(s_arr[j]);
-	        }
+	    public int addNumbers(int num1, int num2) {
+	    	return num1+num2;
 	    }
-	}
+	    public String addStrings(String s1, String s2) {
+	    	return s1+s2;
+	    }
+}
 
